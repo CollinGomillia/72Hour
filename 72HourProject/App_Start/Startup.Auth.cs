1da4ec9897677a139c8d6add72d451a45d6c8c3e
CollinGomillia <collinlgomillia@gmail.com>
2022-01-27 17:06:06 -0500

    Final

--- 72HourProject/App_Start/Startup.Auth.cs
+++ 72HourProject/App_Start/Startup.Auth.cs
@@ -23,7 +23,7 @@ namespace _72HourProject
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(_72Hour.Data.ApplicationUser.ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
